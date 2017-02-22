d16c333f08cdcb4bf06ac363069716091090c970
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2017-02-22 13:24:25 -0500

    started

--- BList.java
+++ BList.java
@@ -1,10 +1,17 @@
 package soen331_a2;
 import be.ac.ua.ansymo.adbc.annotations.*;
 
-@invariant({"$super"})
-public class BList extends List{
-	public BList(){
+@invariant({"$super",
+			"$this.size()<="})
+public class BList<E> extends List<E>{
+	
+	private int capacity;
+	
+	@requires({"capacity > 0",
+			   ""})
+	public BList(int capacity){
 		super();
+		this.capacity = capacity;
 	
 	}
 
