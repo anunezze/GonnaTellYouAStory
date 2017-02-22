1dbdd7b76d44dde163899819bf40a184380db4c2
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2017-02-22 13:50:29 -0500

    to be review

--- BList.java
+++ BList.java
@@ -1,18 +1,23 @@
 package soen331_a2;
 import be.ac.ua.ansymo.adbc.annotations.*;
 
-@invariant({"$super",
-			"$this.size()<="})
+@invariant({"$super && $this.size() <= $this.capacity"})
 public class BList<E> extends List<E>{
 	
 	private int capacity;
 	
-	@requires({"capacity > 0",
-			   ""})
+	@requires({"capacity > 0"})
 	public BList(int capacity){
 		super();
 		this.capacity = capacity;
-	
+	}
+	@requires({"$super && $this.size() <= capacity"})
+	public boolean add(Node<E> node){
+		super.add(node);
+	}
+	@requires({"$super && this.size() <= capacity"})
+	public void add(int index, Node<E> node){
+		super.add(index, node);
 	}
 
 }
