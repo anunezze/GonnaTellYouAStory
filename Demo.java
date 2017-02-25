e8ba57cc4dee178f6388f76fbce12c076e53c117
Eglen Cecaj <eglen@live.com>
2017-02-24 22:56:23 -0500

    works :) (still has to be tested)

--- Demo.java
+++ Demo.java
@@ -1,8 +1,23 @@
 package soen331_a2;
 
 public class Demo {
-	public static <E> void main(String[] args){
-		BList bl = new BList();
+	public static void main(String[] args){
+		Node <String> n1= new Node<String>("Start");
+		Node <String> n2= new Node<String>("Just");
+		Node <String> n3= new Node<String>("do");
+		Node <String> n4= new Node<String>("it");
+		Node <String> n5= new Node<String>("Gousami");
+		Node <String> n6= new Node<String>("Dean");
+		Node <String> n7= new Node<String>("Lucy");
+		Node <String> n8= new Node<String>("David");
 		
+		List l1 = new List(n1);
+		System.out.println("First node in the list: " + n1.getData());
+		
+		l1.add(n2);
+		l1.add(n3);
+		l1.add(n4);
+		System.out.println(l1.isEmpty());
+		System.out.println(l1.indexOf(n3));
 	}
 }
