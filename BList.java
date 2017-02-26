8bbed7377ac5c04a335c8aeffc430cb54f609afb
Eglen Cecaj <eglen@live.com>
2017-02-26 14:45:28 -0500

    testing

--- BList.java
+++ BList.java
@@ -1,17 +1,17 @@
 package a2p;
 import be.ac.ua.ansymo.adbc.annotations.*;
 
-@invariant({"$super && $this.size() <= $this.capacity"})
+//@invariant({"$super && $this.size() <= $this.capacity"})
 public class BList<E> extends List<E>{
 	
 	private int capacity;
 	
-	@requires({"capacity > 0"})
+	//@requires({"capacity > 0"})
 	public BList(int capacity){
 		super();
 		this.capacity = capacity;
 	}
-	@requires({"$super && $this.size() <= capacity"})
+	//@requires({"$super && $this.size() <= capacity"})
 	public boolean add (Node<E> node){
 		try{
 		super.add(node);
@@ -21,7 +21,7 @@ public class BList<E> extends List<E>{
 		}
 		return true;
 	}
-	@requires({"$super && $this.size() <= capacity"})
+	//@requires({"$super && $this.size() <= capacity"})
 	public void add(int index, Node<E> node){
 		super.add(index, node);
 	}
--- Demo.java
+++ Demo.java
@@ -34,5 +34,32 @@ public class Demo {
 		System.out.println(l1.contains(n7));
 		l1.clear();
 		System.out.println(l1.size());
+		
+		//Testing BList
+		System.out.println("");
+		System.out.println("*********** BList *************");
+		System.out.println("Testing BList");
+		BList<String> b1 = new BList<String>(10);
+		b1.add(n2);
+		b1.add(n3);
+		b1.add(n4);
+		b1.add(n5);
+		b1.add(n6);
+		b1.add(2,n7);
+		b1.toStringAll();
+		System.out.println("Checking if the list is empty: " + b1.isEmpty());
+		System.out.println("Index of " + n4.getData() + ": " + b1.indexOf(n4));
+		System.out.println("Size of list is: " + b1.size());
+		System.out.println("Removing node at index 5:");
+		b1.remove(5);
+		System.out.println("Printing out the list:");
+		b1.toStringAll();
+		System.out.println("Size of the list: " + b1.size());
+		System.out.println("Index of node n4: "+ b1.indexOf(n4));
+		System.out.println("Get the data at node n2" + b1.get(2).getData());
+		System.out.println("Checking if the list contains node n7: "+b1.contains(n7));
+		System.out.println("Clearing the list...");
+		b1.clear();
+		System.out.println("Size of BList after clear is: " + b1.size());
 	}
 }
